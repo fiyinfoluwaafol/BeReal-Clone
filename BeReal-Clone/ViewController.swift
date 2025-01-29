8e9d55a18187d7e7c56c5ec7494b2afd57271bd8
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2025-01-29 06:18:08 -0500

    project initialization and setup

---
+++ BeReal-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReal-Clone
+//
+//  Created by Fiyinfoluwa Afolayan on 1/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
