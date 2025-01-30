95e116afcd86d8d49f3853c495046b22b44dd7cb
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2025-01-30 13:42:35 -0500

    implemented user signup & login

---
+++ BeReal-Clone/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  BeReal-Clone
+//
+//  Created by Fiyinfoluwa Afolayan on 1/29/25.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
