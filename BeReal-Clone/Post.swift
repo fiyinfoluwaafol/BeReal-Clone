f591e2bcf145c8a45c9144186f9d5ae70d49cf7e
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2025-01-29 22:59:20 -0500

    defined the data models for Users & Posts

---
+++ BeReal-Clone/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  BeReal-Clone
+//
+//  Created by Fiyinfoluwa Afolayan on 1/29/25.
+//
+
+import Foundation
+import ParseSwift
+
+struct Post: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
+    var imageLocation: ParseGeoPoint?
+}
