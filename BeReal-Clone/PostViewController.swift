9f68cfaae571dcecf3486b9858cfe1e5248bbb83
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2025-02-04 13:38:45 -0500

    changed picking photo from library to camera

--- BeReal-Clone/PostViewController.swift
+++ BeReal-Clone/PostViewController.swift
@@ -23,26 +23,29 @@ class PostViewController: UIViewController {
     }
     
     @IBAction func onPickedimageTapped(_ sender: Any) {
-        // Create a configuration object
-        var config = PHPickerConfiguration()
+        // Make sure the user's camera is available
+        // NOTE: Camera only available on physical iOS device, not available on simulator.
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("❌📷 Camera not available")
+            return
+        }
 
-        // Set the filter to only show images as options (i.e. no videos, etc.).
-        config.filter = .images
+        // Instantiate the image picker
+        let imagePicker = UIImagePickerController()
 
-        // Request the original file format. Fastest method as it avoids transcoding.
-        config.preferredAssetRepresentationMode = .current
+        // Shows the camera (vs the photo library)
+        imagePicker.sourceType = .camera
 
-        // Only allow 1 image to be selected at a time.
-        config.selectionLimit = 1
+        // Allows user to edit image within image picker flow (i.e. crop, etc.)
+        // If you don't want to allow editing, you can leave out this line as the default value of `allowsEditing` is false
+        imagePicker.allowsEditing = true
 
-        // Instantiate a picker, passing in the configuration.
-        let picker = PHPickerViewController(configuration: config)
+        // The image picker (camera in this case) will return captured photos via it's delegate method to it's assigned delegate.
+        // Delegate assignee must conform and implement both `UIImagePickerControllerDelegate` and `UINavigationControllerDelegate`
+        imagePicker.delegate = self
 
-        // Set the picker delegate so we can receive whatever image the user picks.
-        picker.delegate = self
-
-        // Present the picker
-        present(picker, animated: true)
+        // Present the image picker (camera)
+        present(imagePicker, animated: true)
     }
     
     
@@ -100,45 +103,22 @@ class PostViewController: UIViewController {
     
 }
 
-extension PostViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        // Dismiss the picker
-        picker.dismiss(animated: true)
+extension PostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Dismiss the image picker
+            picker.dismiss(animated: true)
 
-        // Make sure we have a non-nil item provider
-        guard let provider = results.first?.itemProvider,
-           // Make sure the provider can load a UIImage
-           provider.canLoadObject(ofClass: UIImage.self) else { return }
+            // Get the edited image from the info dictionary (if `allowsEditing = true` for image picker config).
+            // Alternatively, to get the original image, use the `.originalImage` InfoKey instead.
+            guard let image = info[.editedImage] as? UIImage else {
+                print("❌📷 Unable to get image")
+                return
+            }
 
-        // Load a UIImage from the provider
-        provider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
+            // Set image on preview image view
+            previewImageView.image = image
 
-           // Make sure we can cast the returned object to a UIImage
-           guard let image = object as? UIImage else {
-
-              // ❌ Unable to cast to UIImage
-              self?.showAlert()
-              return
-           }
-
-           // Check for and handle any errors
-           if let error = error {
-               self?.showAlert(description: error.localizedDescription)
-              return
-           } else {
-
-              // UI updates (like setting image on image view) should be done on main thread
-              DispatchQueue.main.async {
-
-                 // Set image on preview image view
-                 self?.previewImageView.image = image
-
-                 // Set image to use when saving post
-                 self?.pickedImage = image
-              }
-           }
-        }
+            // Set image to use when saving post
+            pickedImage = image
     }
-    
-
 }
